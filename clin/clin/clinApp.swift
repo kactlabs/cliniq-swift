6911e382da04b9cf1ed4b69c2a43f23ed95b1cf9
Vipranan <vipranancr7@gmail.com>
2026-01-10 01:17:31 +0530

    ios-template

---
+++ clin/clin/clinApp.swift
@@ -0,0 +1,17 @@
+//
+//  clinApp.swift
+//  clin
+//
+//  Created by Vipranan on 10/01/26.
+//
+
+import SwiftUI
+
+@main
+struct clinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
